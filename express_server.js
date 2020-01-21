20838dcc7eca074ab475cc73fdf8d361762e2f23
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-01-21 03:17:54 +0000

    added a POST request, body-parser and random string generator

--- express_server.js
+++ express_server.js
@@ -1,6 +1,26 @@
+// creation of a simple server using Express for node JS
+
+// generate a string of 6 random alphanumeric characters
+const generateRandomString = function() {
+  const alphanum = 'abcdefghijklmnopqrstuvwxyn0123456789';
+  const numLetters = alphanum.length;
+  const lengthOfString = 6;
+  let outputString = '';
+  for (let i = 0; i < lengthOfString; i++) {
+    const randomNum = Math.round(Math.random() * (numLetters - 1));
+    const selectedLetter = alphanum[randomNum];
+    outputString += selectedLetter;
+  }
+  return outputString;
+};
+
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const PORT = 8080; // default port 8080
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,9 +39,16 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
