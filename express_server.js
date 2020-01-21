aa7c956ae342894eb8a6c140c252f1fe9dceee4a
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-01-21 03:49:38 +0000

    added redirection. we now have some semblance of a tiny URL generator and rediction

--- express_server.js
+++ express_server.js
@@ -21,7 +21,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = 8080; // default port 8080
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -46,9 +45,15 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const longURL = req.body;
+  const tinyURL = generateRandomString();
+  urlDatabase[tinyURL] = longURL;
+  res.redirect('/urls/:' + tinyURL);
 });
 
 app.listen(PORT, () => {
