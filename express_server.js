532b33822fba996fc4db7e5ecc39f8e50631e5d5
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-01-22 16:18:57 +0000

    FINALLY got basic functionality of Tiny App working

--- express_server.js
+++ express_server.js
@@ -28,45 +28,66 @@ const urlDatabase = {
 
 app.set('view engine', 'ejs');
 
+// default home page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-//
+// hello page
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-//
+// listing of available URLs
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-//
+// adding a new URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-//
+// edit an existing URL
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-//
+// get directed to actual website
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
-// generate a tinyURL based on input longURL
+// generate a tinyURL for longURL
 app.post("/urls", (req, res) => {
-  const longURL = req.body;
-  const tinyURL = generateRandomString();
-  urlDatabase[tinyURL] = longURL;
-  res.redirect('/urls/:' + tinyURL);
+  const longURL = req.body.longURL;
+  console.log('longURL :', longURL);
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  console.log('urlDatabase :', urlDatabase);
+  // res.redirect('/urls/:' + shortURL);
+  res.redirect('/urls/');
 });
 // deletion of URL ink
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];  // why is it not req.body like in "/urls" ?
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];  // why are we not using req.body like in "/urls" ?
   res.redirect('/urls/');
+});
+// updating after editing a long URL
+app.post("/urls/:shortURL/edit", (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls/');
+});
+// edit an existing URL
+app.post("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}! at ${Date()}`);
 });
+
+// TODO
+// pressing EDIT button should go to edit screen
+// when adding a new URL the long URL is not showing up properly
